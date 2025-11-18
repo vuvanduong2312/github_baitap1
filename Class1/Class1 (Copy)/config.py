21e41da68c5806b4e573b2cdfb91abc19c9be4fa
vuvanduong2312 <vuvanduong2312@gmail.com>
2025-11-18 22:57:52 +0700

    cap nhap

--- Class1/Class1 (Copy)/config.py
+++ Class1/Class1 (Copy)/config.py
@@ -1,6 +1,18 @@
 import os
+from dotenv import load_dotenv
+
+# Load biến môi trường từ file .env
+load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-production'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///bookstore.db'
+    # Secret key
+    SECRET_KEY = os.getenv("SECRET_KEY", "defaultsecret")
+
+    # Database URI (MySQL Docker)
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
+
+    # Tắt warning
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Flask mode
+    ENV = os.getenv("FLASK_ENV", "production")
